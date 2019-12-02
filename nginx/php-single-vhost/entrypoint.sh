210dd7b9a3a0835561343214ead75317c83801e1
Antoine Gomez <antoine@gomez.lu>
2019-12-02 17:45:16 +0000

    Add variables to configure gzip, max client size

--- nginx/php-single-vhost/entrypoint.sh
+++ nginx/php-single-vhost/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${PHP_PUBLIC_DIR} ${PHP_HOST}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+GZIP=${GZIP:=on}
+GZIP_VARY=${GZIP_VARY:=on}
+GZIP_PROXIED=${GZIP_PROXIED:=any}
+GZIP_TYPES=${GZIP_TYPES:=text/plain text/css text/xml text/javascript application/x-javascript application/xml application/json}
+CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:=1}
+PHP_PUBLIC_DIR=${PHP_PUBLIC_DIR:=/var/www/html}
+PHP_HOST=${PHP_HOST:=localhost}
+PHP_PORT=${PHP_PORT:=9000}
+
+envsubst '${GZIP} ${GZIP_VARY} ${GZIP_PROXIED} ${GZIP_TYPES} ${CLIENT_MAX_BODY_SIZE} ${PHP_PUBLIC_DIR} ${PHP_HOST} ${PHP_PORT}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
