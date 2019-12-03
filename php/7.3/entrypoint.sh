f6e52b4a94b7716737c3df7343c7498dbd14c341
Antoine Gomez <antoine@gomez.lu>
2019-12-03 16:24:32 +0000

    Update PHP image

---
+++ php/7.3/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -eu
+
+export FILE_UPLOADS=${FILE_UPLOADS:-On}
+export MEMORY_LIMIT=${MEMORY_LIMIT:-32}
+export UPLOAD_MAX_FILE_SIZE=${UPLOAD_MAX_FILE_SIZE:-1}
+export POST_MAX_SIZE=${POST_MAX_SIZE:-2}
+
+envsubst '${CLIENT_MAX_BODY_SIZE} ${FILE_UPLOADS} ${MEMORY_LIMIT} ${UPLOAD_MAX_FILE_SIZE} ${POST_MAX_SIZE}' < /usr/local/etc/php/conf.d/php-upload.template > /usr/local/etc/php/conf.d/php-upload.ini
+
+echo "Patched /usr/local/etc/php/conf.d/php-upload.ini"
+
+exec "$@"
